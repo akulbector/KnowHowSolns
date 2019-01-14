ac844d56ead84c8c96d6f09b90d15dbd0e458b9f
akulbector <40412673+akulbector@users.noreply.github.com>
2019-01-14 09:34:28 -0500

    Update and rename Nested Loops.py to 2B-Nested_Loops.py

---
+++ 2B-Nested_Loops.py
@@ -0,0 +1,7 @@
+for i in range(1,11):
+    for j in range(2,5):
+        #Prints the value of the numbers 1-10 raised to the power of 2, then 3, then 4
+        print(i**j, end="   |   ")
+        if(j==4):
+            print("")
+            #Go to Next Line
--- Nested Loops.py
+++
@@ -1,5 +0,0 @@
-for i in range(1,11):
-    for j in range(2,5):
-        print(i**j, end="   |   ")
-        if(j==4):
-            print("")
