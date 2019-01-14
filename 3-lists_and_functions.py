19cba43ddf62b9ce02dc6554ebafc5e53e849eb4
akulbector <40412673+akulbector@users.noreply.github.com>
2019-01-14 09:40:06 -0500

    Update and rename lists_and_functions.py to 3-lists_and_functions.py

--- 3-lists_and_functions.py
+++ 3-lists_and_functions.py
@@ -3,48 +3,48 @@ grades2 = []
 grades3 = []
 grades4 = []
 
-
+#Averages all grades above 49
 def avgPassingGrades(inputList):
-    sublist = []
+    #Creates a sum and count variable since sum/count = average
     count = 0
     sum = 0
     
     
     #for element in inputList
     for i in range(0, len(inputList)):
+        #If the element is passing grade
         #if(element>49):
         if(inputList[i]>49):
+            #Then include it in the average calculation
             #sum += element
             sum += inputList[i]
             count +=1
             
     return sum/count
 
-
+#Returns the lowest grade above 49
+#If there is no grade above 49, function returns false
 def lowest(list):
-    isThereALowest = False
+    #Sets lowest=False initially, since if there is no grade above 49, the function will return false
     lowest=False
     
     for element in list:
         if(element>49):
-            if (lowest==False):
-                lowest=element
-            elif (lowest>element):
+            if (lowest==False or lowest>element):
                 lowest=element
                 
                 
     return lowest
             
-            
+#Sorts all passing grades            
 def sorting(list):
+    #List to output
     sortedList = []
+    #Creates a copy to keep the original list untouched
     copy = list.copy()
     
-    #for element in copy:
-     #   if (element<49):
-      #      copy.remove(element)
-    
-    
+   
+    #Repeatedly finds the lowest passing grade, removes it from copy of the input list, and appends it into the output list
     while (len(copy)>0):
         minVal = lowest(copy)
         copy.remove(minVal)
